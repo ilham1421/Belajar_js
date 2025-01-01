b3a854f9fe20daae166cfdf002b55e172faab268
ilham1421 <105841105822@student.unismuh.ac.id>
2025-01-01 18:26:06 +0800

    pembelajaran kedua

---
+++ belajar2.js
@@ -0,0 +1,8 @@
+console.log("ini adalah file belajar2.js");
+console.log("ini javascript pertama saya");
+
+//ini adalah komentar (//)
+// console.log('Hello World!');
+ const PI = '3.14';
+ console.log(PI);
+ 
