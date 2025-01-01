b1f44329a57ba630232d38da5d80c25a680ce6d0
ilham1421 <105841105822@student.unismuh.ac.id>
2025-01-01 18:37:43 +0800

    pembelajaran ketiga

---
+++ belajar3.js
@@ -0,0 +1,20 @@
+// const id = 123;
+// let username = "MIA";
+
+// console.log(id);
+// console.log(username);
+
+//variabel const tidak bisa diubah nilainya
+//variabel let bisa diubah nilainya
+
+let username = "MIA";
+console.log("sebelum dirubah: ", username);
+
+username = "MIA 2";
+console.log("setelah dirubah: ", username);
+
+//variabel const tidak bisa diubah nilainya
+const id = 123;
+console.log(id);
+id = 456; //error
+console.log(id); //error
